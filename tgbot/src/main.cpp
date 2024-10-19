d0d66d7a26187c35ea14b4ff706d827f84e8d0c5
Arseny Ravin <arseny802@yandex.ru>
2024-10-20 00:35:37 +0300

    removed token

--- tgbot/src/main.cpp
+++ tgbot/src/main.cpp
@@ -2,30 +2,32 @@
 // #include <tgbot/tgbot.h>
 
 namespace yw::master_bot {
-  int bot_example() {
-    /*TgBot::Bot bot("7297850224:AAHfuN9L0NehxNFrCbpVsTbbKeKMfv3Vnbc");
-    bot.getEvents().onCommand(
-        "start", [&bot](TgBot::Message::Ptr message) { bot.getApi().sendMessage(message->chat->id, "Hi!"); });
-    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
-      log()->info("User wrote {}\n", message->text.c_str());
-      if (StringTools::startsWith(message->text, "/start")) {
-        return;
-      }
-      bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
-    });
-    try {
-      log()->info("Bot username: {}\n", bot.getApi().getMe()->username.c_str());
-      TgBot::TgLongPoll longPoll(bot);
-      while (true) {
-        log()->info("Long poll started\n");
-        longPoll.start();
-      }
-    } catch (TgBot::TgException& e) {
-      log()->error("error: %s\n", e.what());
-    }*/
-    return 0;
-  }
-} // namespace yw::master_bot
+int bot_example() {
+  /*TgBot::Bot bot("TOKEN");
+  bot.getEvents().onCommand(
+      "start", [&bot](TgBot::Message::Ptr message) {
+  bot.getApi().sendMessage(message->chat->id, "Hi!"); });
+  bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+    log()->info("User wrote {}\n", message->text.c_str());
+    if (StringTools::startsWith(message->text, "/start")) {
+      return;
+    }
+    bot.getApi().sendMessage(message->chat->id, "Your message is: " +
+  message->text);
+  });
+  try {
+    log()->info("Bot username: {}\n", bot.getApi().getMe()->username.c_str());
+    TgBot::TgLongPoll longPoll(bot);
+    while (true) {
+      log()->info("Long poll started\n");
+      longPoll.start();
+    }
+  } catch (TgBot::TgException& e) {
+    log()->error("error: %s\n", e.what());
+  }*/
+  return 0;
+}
+}  // namespace yw::master_bot
 
 int main() {
   yw::master_bot::initialize_logging();
